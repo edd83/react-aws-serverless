f63614fee08cbbe6f6e97917b64afacc23ab3dea
Eddy Lardet <eddy.lardet@zappar.com>
2018-08-20 09:54:30 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "notes-app-uploads-test-eddy"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://p9asmo2cha.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_NZDtEIx95",
+        APP_CLIENT_ID: "648rssqeltiviql0hr955eanil",
+        IDENTITY_POOL_ID: "eu-west-1:41665105-a51a-4b0b-b9e3-fbebf259f972"
+    }
+};
